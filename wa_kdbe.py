e2ea98f2dd0aa622a4ec587dd792179351a5b465
Robert S <31773450+roberts01@users.noreply.github.com>
2021-02-25 14:54:45 +0100

    Cleared up programming, removed nearly all the defs

--- wa_kdbe.py
+++ wa_kdbe.py
@@ -13,7 +13,7 @@ import re
 # SDPath = '' # Internal storage.
 # versionName = ''
 # contentLength = '' # To check if APK even exists at a given path to download!
-appURLWhatsAppCDN = 'https://www.cdn.whatsapp.net/android/2.11.431/WhatsApp.apk'
+#appURLWhatsAppCDN = 'https://www.cdn.whatsapp.net/android/2.11.431/WhatsApp.apk' broken
 appURLWhatsCryptCDN = 'https://whatcrypt.com/WhatsApp-2.11.431.apk'
 isJAVAInstalled = False
 ADBSerialId = deviceId.init()
@@ -27,35 +27,6 @@ grep = 'grep'
 curl = 'curl'
 helpers = 'helpers/'
 extracted = 'extracted/'
-
-def main() :
-    os.system('clear')
-    CheckBin()
-    ShowBanner()
-    global isJAVAInstalled
-    #isJAVAInstalled = CheckJAVA()
-    CustomPrint('Temporarily continuing without Java.')
-    USBMode()
-
-def BackupWhatsAppApk(SDKVersion, versionName, WhatsAppapkPath):
-    os.system(adb + ' shell am force-stop com.whatsapp') if(SDKVersion > 11) else os.system(adb + ' shell am kill com.whatsapp')
-    CustomPrint('Backing up WhatsApp ' + versionName + ' apk, the one installed on device to ' + tmp + 'WhatsAppbackup.apk')
-    os.system(adb + ' pull ' + WhatsAppapkPath + ' ' + tmp + 'WhatsAppbackup.apk')
-    CustomPrint('Apk backup complete.')
-
-def BackupWhatsAppDataasAb(SDKVersion):
-    CustomPrint('Backing up WhatsApp data as ' + tmp + 'whatsapp.ab. May take time, don\'t panic.')
-    try : 
-        os.system(adb + ' backup -f '+ tmp + 'whatsapp.ab com.whatsapp') if(SDKVersion >= 23) else os.system(adb + ' backup -f '+ tmp + 'whatsapp.ab -noapk com.whatsapp')
-    except Exception as e : 
-        CustomPrint(e)
-    CustomPrint('Done backing up data.')
-
-def CheckBin() : 
-    if (not os.path.isdir('bin')) : 
-        CustomPrint('I can not find bin folder, check again...', 'green')
-        Exit()
-    pass
 
 def CheckJAVA() : 
     JAVAVersion = re.search('(?<=version ")(.*)(?=")', str(subprocess.check_output('java -version'.split(), stderr=subprocess.STDOUT))).group(1)
@@ -76,39 +47,15 @@ def Exit():
     os.system('adb kill-server')
     quit()
 
-def InstallLegacy(SDKVersion):
-    CustomPrint("installing Legacy WhatsApp V2.11.431, hold tight now.")
-    if(SDKVersion >= 17) :
-        os.system(adb + ' install -r -d '+ helpers + 'LegacyWhatsApp.apk')
-    else : 
-        os.system(adb + ' install -r '+ helpers + 'LegacyWhatsApp.apk')
-    CustomPrint('Installation Complete.')
+if __name__ == "__main__":
+    os.system('clear')
+    #Check for bin folder
+    if (not os.path.isdir('bin')) : 
+        CustomPrint('I can not find bin folder, check again...', 'green')
+        Exit()
+    pass
 
-def RealDeal(SDKVersion, WhatsAppapkPath, versionName) : 
-    BackupWhatsAppApk(SDKVersion, versionName, WhatsAppapkPath)
-    UninstallWhatsApp(SDKVersion)
-    InstallLegacy(SDKVersion)
-    BackupWhatsAppDataasAb(SDKVersion)
-    ReinstallWhatsApp()
-    CustomPrint('Our work with device has finished.')
-    # ExtractAB(isJAVAInstalled)
-    CustomPrint('Extraction is not possible on termux as of now. I have to back \'whatsapp.ab\' up in \'extracted\' folder.')
-    userName = CustomInput('Enter a reference name for this user (Remeber this name for later). : ') or 'user'
-    os.mkdir(extracted + userName) if not (os.path.isdir(extracted + userName)) else CustomPrint('Folder already exists.')
-    # copy from to here.
-    os.system('mv ' + tmp + 'whatsapp.ab ' + extracted + userName + '/whatsapp.ab')
-    CustomPrint('Done copying, deleting from \'tmp\' folder. Now run \'view_extract.py\' from computer.')
-    os.system('rm -rf ' + tmp + 'whatsapp.ab')
-
-def ReinstallWhatsApp():
-    CustomPrint('Reinstallting original WhatsApp.')
-    try : 
-        os.system(adb + ' install -r -d ' + tmp + 'WhatsAppbackup.apk')
-    except Exception as e : 
-        print(e)
-        CustomPrint('Could not install WhatsApp, install by running \'restore_whatsapp.py\' or manually installing from Play Store.\nHowever if it crashes then you have to clear storage/clear data from settings => app settings => WhatsApp.')
-
-def ShowBanner() : 
+    #Show Banner
     banner_path = 'non_essentials/banner.txt'
     try : 
         banner = open(banner_path,'r')
@@ -126,21 +73,65 @@ def ShowBanner() :
         intro.close()
     except Exception as e : 
         CustomPrint(e)
-
-def UninstallWhatsApp(SDKVersion):
-    if(SDKVersion >= 23) :
-        try : 
-            CustomPrint('Uninstalling WhatsApp, skipping data.')
-            os.system(adb + ' shell pm uninstall -k com.whatsapp')
-            CustomPrint('Uninstalled.')
-        except Exception as e : 
-            CustomPrint('Could not uninstall WhatsApp.')
-            CustomPrint(e)
-            Exit()
-
-def USBMode() :  
+        
+    global isJAVAInstalled
+    #isJAVAInstalled = CheckJAVA()
+    CustomPrint('Temporarily continuing without Java.')
+    
+    #USB Mode
     ACReturnCode, SDKVersion, WhatsAppapkPath, versionName =  TermuxMode(ADBSerialId)
-    RealDeal(SDKVersion, WhatsAppapkPath, versionName) if ACReturnCode==1 else Exit()
+    
+    #If works, start Whatapp operations
+    if ACReturnCode==1:
+        #BackupWhatsAppApk
+        os.system(adb + ' shell am force-stop com.whatsapp') if(SDKVersion > 11) else os.system(adb + ' shell am kill com.whatsapp')
+        CustomPrint('Backing up WhatsApp ' + versionName + ' apk, the one installed on device to ' + tmp + 'WhatsAppbackup.apk')
+        os.system(adb + ' pull ' + WhatsAppapkPath + ' ' + tmp + 'WhatsAppbackup.apk')
+        CustomPrint('Apk backup complete.')
 
-if __name__ == "__main__":
-    main()
+        #UninstallWhatsApp
+        if(SDKVersion >= 23) :
+            try : 
+                CustomPrint('Uninstalling WhatsApp, skipping data.')
+                os.system(adb + ' shell pm uninstall -k com.whatsapp')
+                CustomPrint('Uninstalled.')
+            except Exception as e : 
+                CustomPrint('Could not uninstall WhatsApp.')
+                CustomPrint(e)
+                Exit()
+
+        #InstallLegacyWhatsapp
+        CustomPrint("installing Legacy WhatsApp V2.11.431, hold tight now.")
+        if(SDKVersion >= 17) :
+            os.system(adb + ' install -r -d '+ helpers + 'LegacyWhatsApp.apk')
+        else : 
+            os.system(adb + ' install -r '+ helpers + 'LegacyWhatsApp.apk')
+        CustomPrint('Installation Complete.')
+
+        #Backup WhatsApp Data as .ab File
+        CustomPrint('Backing up WhatsApp data as ' + tmp + 'whatsapp.ab. May take time, don\'t panic.')
+        try : 
+            os.system(adb + ' backup -f '+ tmp + 'whatsapp.ab com.whatsapp') if(SDKVersion >= 23) else os.system(adb + ' backup -f '+ tmp + 'whatsapp.ab -noapk com.whatsapp')
+        except Exception as e : 
+            CustomPrint(e)
+        CustomPrint('Done backing up data.')
+
+        #ReinstallWhatsApp
+        CustomPrint('Reinstallting original WhatsApp.')
+        try : 
+            os.system(adb + ' install -r -d ' + tmp + 'WhatsAppbackup.apk')
+        except Exception as e : 
+            print(e)
+            CustomPrint('Could not install WhatsApp, install by running \'restore_whatsapp.py\' or manually installing from Play Store.\nHowever if it crashes then you have to clear storage/clear data from settings => app settings => WhatsApp.')    
+
+        CustomPrint('Our work with device has finished.')
+        # ExtractAB(isJAVAInstalled)
+        CustomPrint('Extraction is not possible on termux as of now. I have to back \'whatsapp.ab\' up in \'extracted\' folder.')
+        userName = CustomInput('Enter a reference name for this user (Remeber this name for later). : ') or 'user'
+        os.mkdir(extracted + userName) if not (os.path.isdir(extracted + userName)) else CustomPrint('Folder already exists.')
+        # copy from to here.
+        os.system('mv ' + tmp + 'whatsapp.ab ' + extracted + userName + '/whatsapp.ab')
+        CustomPrint('Done copying, deleting from \'tmp\' folder. Now run \'view_extract.py\' from computer.')
+        os.system('rm -r -f ' + tmp + 'whatsapp.ab') 
+    else:
+        Exit()
