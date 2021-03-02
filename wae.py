cf988a5ec55d9c33afb8c3c7217be4b88f33d1d8
Robert S <31773450+roberts01@users.noreply.github.com>
2021-03-02 12:45:23 +0100

    Update wae.py

--- wae.py
+++ wae.py
@@ -1,6 +1,6 @@
 import os
 import re
-import subprocess
+import subprocess as sp
 from termcolor import colored, cprint
 from view_extract import ExtractAB
 from packaging import version
@@ -16,7 +16,8 @@ appURLWhatsCryptCDN = 'https://whatcrypt.com/WhatsApp-2.11.431.apk'
 isJAVAInstalled = False
 
 # Global command line helpers
-adb = 'adb -s ' + ADBSerialId
+#adb = 'adb -s ' + ADBSerialId
+adb = 'adb'
 delete = 'rm -r -f'
 tmp = 'tmp/'
 confirmDelete = ''
@@ -31,19 +32,73 @@ def CustomInput(textToInput, color = 'green', attr=[]):
 def CustomPrint(textToPrint, color = 'green', attr=[]): 
     cprint(textToPrint, color, attrs=attr)
 
+def Exit():
+    CustomPrint('\nExiting...', 'green')
+    os.system('adb kill-server')
+    quit()
+
 #former ADBDeviceSerialId.py
 
-    # Global command line helpers
-    currDir = os.path.dirname(os.path.realpath(__file__))
-    rootDir = os.path.abspath(os.path.join(currDir, '..'))
-    os.system('proot login')
-    adb = 'adb'
-    os.system(adb + ' devices')
-    ADBSerialId = CustomInput('Choose device from "List of devices attached"\nFor example : 7835fd84543/emulator-5554 : ', 'green')
+# Global command line helpers
+currDir = os.path.dirname(os.path.realpath(__file__))
+rootDir = os.path.abspath(os.path.join(currDir, '..'))
 
+cmd = adb + ' devices'
+# Kill server before getting list to avoid daemon texts.
+os.system(adb + ' kill-server')
+os.system(adb + ' start-server')
+proc = sp.Popen(cmd.split(), stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE, shell=False)
+output, error = proc.communicate()
+output = output.decode('utf-8')
+error = error.decode('utf-8')
+
+if len(output) == 0 or error:
+    output = None
+    CustomPrint(error, 'red')
+    Exit()
+else:
+    output = [x.strip() for x in output.split('\n') if len(x.strip()) > 0]
+
+if(len(output) == 1):
+    CustomPrint('Could not find any connected device. Is USB Debugging on?', 'red')
+    return ''
+
+deviceToConnect = None
+i = 1
+if(len(output) == 2):
+    if(output[1].split()[1] == 'offline'):
+        CustomPrint('Device is offline, try turning off USB debugging and turn on again.', 'yellow')
+        Exit()
+    if(output[1].split()[1] == 'unauthorized'):
+        CustomPrint('Device unauthorized. Please check the confirmation dialog on your device.', 'red')
+        Exit()
+    return output[1].split()[0]
+
+CustomPrint(output[0])
+print('\n')
+if deviceToConnect is None:
+    for device in output[1:]:
+        name = adb + ' -s ' + \
+        device.split()[0] + ' shell getprop ro.product.model'
+        CustomPrint(str(i) + '. ' + device.split()[0] + '  ' + device.split()[1] + '  ' + sp.getoutput(name).strip())
+        i += 1
+
+while deviceToConnect is None:
+    deviceIndex = int(CustomInput('Enter device number (for ex : 2) : '))
+    if deviceIndex <= 0 or deviceIndex + 1 > len(output):
+        continue
+    deviceToConnect = output[deviceIndex]
+
+if(deviceToConnect.split()[1] == 'offline'):
+    CustomPrint('Device is offline, try turning off USB debugging and turn on again.', 'yellow')
+    Exit()
+if(deviceToConnect.split()[1] == 'unauthorized'):
+    CustomPrint('Device unauthorized. Please check the confirmation dialog on your device.', 'red')
+    Exit()
+ADBSerialId = deviceToConnect.split()[0]
 
 def CheckJAVA() : 
-    JAVAVersion = re.search('(?<=version ")(.*)(?=")', str(subprocess.check_output('java -version'.split(), stderr=subprocess.STDOUT))).group(1)
+    JAVAVersion = re.search('(?<=version ")(.*)(?=")', str(sp.check_output('java -version'.split(), stderr=sp.STDOUT))).group(1)
     
     if(JAVAVersion):
         isJAVAInstalled = True 
@@ -54,17 +109,8 @@ def CheckJAVA() :
         CustomPrint('Found Java installed on system. Continuing...')
         return isJAVAInstalled
     else: 
-        noJAVAContinue = CustomInput('It looks like you don\'t have JAVA installed on your system. Would you like to (C)ontinue with the process and \'view extract\' later? or (S)top? : ', 'green') or 'c'
-        if(noJAVAContinue=='c'): 
-            CustomPrint('Continuing without JAVA, once JAVA is installed on system run \'view_extract.py\'', 'green')
-            return isJAVAInstalled
-        else: 
-            Exit()
-
-def Exit():
-    CustomPrint('\nExiting...', 'green')
-    os.system('adb kill-server')
-    quit()
+        CustomPrint('It looks like you don\'t have JAVA installed on your system. Please install it first.')
+        Exit()
 
 #BEGINNING-------------------------------------------------
     
