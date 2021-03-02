577baada8806cc88f24450beb6da6fbfe4edc617
Robert S <31773450+roberts01@users.noreply.github.com>
2021-03-02 10:17:33 +0100

    Update termux_dependencies.py

--- helpers/termux_dependencies.py
+++ helpers/termux_dependencies.py
@@ -1,26 +1,32 @@
 from CustomCI import CustomPrint
 import os
 
-def InstallTermuxDependencies() : 
-    CustomPrint("Installing dependencies...")
-    CustomPrint("Updating Termux")
-    os.system('pkg update && pkg upgrade')
-    CustomPrint("Allow storage permission for storing extracted whatsapp.ab in interal storage:")
-    os.system('termux-setup-storage')
-    CustomPrint("Done. Installing required dependencies...")
-    os.system('pkg install curl grep tar proot wget -y')
-    os.system('pip install termcolor packaging')
-    #os.system('wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh && bash InstallTools.sh')
-    os.system('git clone https://github.com/MasterDevX/Termux-ADB.git && bash InstallTools.sh')
-    try:
-        os.system('rm -r -f installjava') # Deleting any previous instance of installjava.
-    except Exception as e : 
-        pass
-    #os.system('wget https://raw.githubusercontent.com/MasterDevX/java/master/installjava && sh installjava')
-    os.system('git clone https://github.com/MasterDevX/java.git && sh installjava')
-    os.system('proot login')
-    CustomPrint("Connecting ADB with local device:")
-    os.system('adb connect localhost')
-    CustomPrint("Succesfully installed all dependencies.")
+CustomPrint("Installing dependencies...")
 
-InstallTermuxDependencies()
+CustomPrint("Updating Termux")
+os.system('pkg update && pkg upgrade')
+
+CustomPrint("Allow storage permission for storing extracted whatsapp.ab in interal storage:")
+os.system('termux-setup-storage')
+
+CustomPrint("Installing required packaged dependencies...")
+os.system('pkg install curl grep tar proot wget -y')
+os.system('pip install termcolor packaging')
+
+
+#os.system('wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh && bash InstallTools.sh')
+os.system('git clone https://github.com/MasterDevX/Termux-ADB.git && bash InstallTools.sh')
+try:
+    os.system('rm -r -f installjava') # Deleting any previous instance of installjava.
+except Exception as e : 
+    pass
+
+#os.system('wget https://raw.githubusercontent.com/MasterDevX/java/master/installjava && sh installjava')
+#os.system('git clone https://github.com/MasterDevX/Termux-Java.git && sh installjava')
+os.system('git clone https://github.com/roberts01/tjava.git && sh installjava')
+
+os.system('proot login')
+
+CustomPrint("Connecting ADB with local device:")
+os.system('adb connect localhost')
+CustomPrint("Succesfully installed all dependencies.")
