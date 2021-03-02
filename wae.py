b2a982bd5f62cf65e2f3872818f0078fbf906d0d
Robert S <31773450+roberts01@users.noreply.github.com>
2021-03-02 15:59:55 +0100

    Update wae.py

--- wae.py
+++ wae.py
@@ -1,5 +1,6 @@
 import os
 import re
+import subprocess as sp
 
 try:
     import packaging
@@ -11,12 +12,8 @@ except ImportError:
     except:
         os.system('python3 -m pip install --upgrade packaging')
 
-import subprocess as sp
-
 #-------------------------Dependencies-and-prerequisites---------------------------------#
-
-
-print("Installing dependencies...")
+print("Checking and installing dependencies...")
 
 print("Updating Termux")
 os.system('pkg update && pkg upgrade')
@@ -47,9 +44,6 @@ os.system('proot login')
 print("Connecting ADB with local device:")
 os.system('adb connect localhost')
 print("Succesfully installed all dependencies.")
-
-
-
 #---------------Starting-main-porgram-------------------------------------#
 
 
@@ -75,7 +69,7 @@ curl = 'curl'
 extracted = 'extracted/'
 
 def Exit():
-    CustomPrint('\nExiting...', 'green')
+    print('\nExiting...')
     os.system('adb kill-server')
     quit()
 
@@ -96,46 +90,46 @@ error = error.decode('utf-8')
 
 if len(output) == 0 or error:
     output = None
-    CustomPrint(error, 'red')
+    print(error, 'red')
     Exit()
 else:
     output = [x.strip() for x in output.split('\n') if len(x.strip()) > 0]
 
 if(len(output) == 1):
-    CustomPrint('Could not find any connected device. Is USB Debugging on?', 'red')
+    print('Could not find any connected device. Is USB Debugging on?')
     ADBSerialId = ''
 
 deviceToConnect = None
 i = 1
 if(len(output) == 2):
     if(output[1].split()[1] == 'offline'):
-        CustomPrint('Device is offline, try turning off USB debugging and turn on again.', 'yellow')
+        print('Device is offline, try turning off USB debugging and turn on again.')
         Exit()
     if(output[1].split()[1] == 'unauthorized'):
-        CustomPrint('Device unauthorized. Please check the confirmation dialog on your device.', 'red')
+        print('Device unauthorized. Please check the confirmation dialog on your device.')
         Exit()
     ADBSerialId = output[1].split()[0]
 
-CustomPrint(output[0])
+print(output[0])
 print('\n')
 if deviceToConnect is None:
     for device in output[1:]:
         name = adb + ' -s ' + \
         device.split()[0] + ' shell getprop ro.product.model'
-        CustomPrint(str(i) + '. ' + device.split()[0] + '  ' + device.split()[1] + '  ' + sp.getoutput(name).strip())
+        print(str(i) + '. ' + device.split()[0] + '  ' + device.split()[1] + '  ' + sp.getoutput(name).strip())
         i += 1
 
 while deviceToConnect is None:
-    deviceIndex = int(CustomInput('Enter device number (for ex : 2) : '))
+    deviceIndex = int(input('Enter device number (for ex : 2) : '))
     if deviceIndex <= 0 or deviceIndex + 1 > len(output):
         continue
     deviceToConnect = output[deviceIndex]
 
 if(deviceToConnect.split()[1] == 'offline'):
-    CustomPrint('Device is offline, try turning off USB debugging and turn on again.', 'yellow')
+    print('Device is offline, try turning off USB debugging and turn on again.')
     Exit()
 if(deviceToConnect.split()[1] == 'unauthorized'):
-    CustomPrint('Device unauthorized. Please check the confirmation dialog on your device.', 'red')
+    print('Device unauthorized. Please check the confirmation dialog on your device.')
     Exit()
 ADBSerialId = deviceToConnect.split()[0]
 
@@ -148,10 +142,10 @@ def CheckJAVA() :
         isJAVAInstalled = False
         
     if (isJAVAInstalled): 
-        CustomPrint('Found Java installed on system. Continuing...')
+        print('Found Java installed on system. Continuing...')
         return isJAVAInstalled
     else: 
-        CustomPrint('It looks like you don\'t have JAVA installed on your system. Please install it first.')
+        print('It looks like you don\'t have JAVA installed on your system. Please install it first.')
         Exit()
 
 #-------------------------------------------------BEGINNING-------------------------------------------------
@@ -162,32 +156,30 @@ if __name__ == "__main__":
    
     isJAVAInstalled = CheckJAVA()
     
-    CustomPrint('Temporarily continuing without Java.')
-    
     #USB Mode
     #Former Termux.py
     #def TermuxMode(ADBSerialId)
     _deviceName= 'adb -s ' + ADBSerialId + ' shell getprop ro.product.model'
-    CustomPrint('Connected to ' + re.search("(?<=b')(.*)(?=\\\\n)", str(check_output(_deviceName.split()))).group(1) , 'green')
+    print('Connected to ' + re.search("(?<=b')(.*)(?=\\\\n)", str(check_output(_deviceName.split()))).group(1) )
 
     _sdkVersionText = 'adb -s ' + ADBSerialId + ' shell getprop ro.build.version.sdk'
     SDKVersion = int(re.search('[0-9]{2,3}', str(check_output(_sdkVersionText.split()))).group(0))
     if (SDKVersion <= 13) :
-        CustomPrint('Unsupported device. This method only works on Android v4.0 or higer.', 'green')
-        CustomPrint('Cleaning up temporary direcory.', 'green')
+        print('Unsupported device. This method only works on Android v4.0 or higer.')
+        print('Cleaning up temporary direcory.')
         os.system('rm -r -f tmp/*')
         Exit()
         
     _waPathText = 'adb -s ' + ADBSerialId + ' shell pm path com.whatsapp'
     WhatsAppapkPath = re.search('(?<=package:)(.*)(?=apk)', str(check_output(_waPathText.split()))).group(1) + 'apk'
     if not (WhatsAppapkPath): 
-        CustomPrint('Looks like WhatsApp is not installed on device.')
+        print('Looks like WhatsApp is not installed on device.')
         Exit()
     #SDPath = re.search("(?<=b')(.*)(?=\\\\n)", str(check_output('adb shell "echo $EXTERNAL_STORAGE"'.split()))).group(1)
     contentLength = int(re.search("(?<=Content-Length:)(.*[0-9])(?=)", str(check_output('curl -sI https://whatcrypt.com/WhatsApp-2.11.431.apk'.split()))).group(1))
     _versionNameText = 'adb -s ' + ADBSerialId + ' shell dumpsys package com.whatsapp'
     versionName = re.search("(?<=versionName=)(.*?)(?=\\\\n)", str(check_output(_versionNameText.split()))).group(1)
-    CustomPrint('WhatsApp Version' + versionName + ' installed on device') 
+    print('WhatsApp Version' + versionName + ' installed on device') 
     ACReturnCode = 1
     
     #If works, start Whatapp operations
@@ -199,49 +191,49 @@ if __name__ == "__main__":
         else:
             os.system(adb + ' shell am kill com.whatsapp')
             
-        CustomPrint('Backing up WhatsApp ' + versionName + ' apk, the one installed on device to ' + tmp + 'WhatsAppbackup.apk')
+        print('Backing up WhatsApp ' + versionName + ' apk, the one installed on device to ' + tmp + 'WhatsAppbackup.apk')
         os.system(adb + ' pull ' + WhatsAppapkPath + ' ' + tmp + 'WhatsAppbackup.apk')
-        CustomPrint('Apk backup complete.')
+        print('Apk backup complete.')
 
         #UninstallWhatsApp
         if(SDKVersion >= 23) :
             try : 
-                CustomPrint('Uninstalling WhatsApp, skipping data.')
+                print('Uninstalling WhatsApp, skipping data.')
                 os.system(adb + ' shell pm uninstall -k com.whatsapp')
-                CustomPrint('Uninstalled.')
+                print('Uninstalled.')
             except Exception as e : 
-                CustomPrint('Could not uninstall WhatsApp.')
-                CustomPrint(e)
+                print('Could not uninstall WhatsApp.')
+                print(e)
                 Exit()
 
         #InstallLegacyWhatsapp
-        CustomPrint("installing Legacy WhatsApp v2.11.431...")
+        print("installing Legacy WhatsApp v2.11.431...")
         if(SDKVersion >= 17) :
             os.system(adb + ' install -r -d WhatsApp-2.11.431.apk')
         else : 
             os.system(adb + ' install -r WhatsApp-2.11.431.apk')
-        CustomPrint('Installation Complete.')
+        print('Installation Complete.')
 
         #Backup WhatsApp Data as .ab File
-        CustomPrint('Backing up WhatsApp data as ' + tmp + 'whatsapp.ab...')
+        print('Backing up WhatsApp data as ' + tmp + 'whatsapp.ab...')
         try:
             if(SDKVersion >= 23):
                 os.system(adb + ' backup -f '+ tmp + 'whatsapp.ab com.whatsapp')
             else:
                 os.system(adb + ' backup -f '+ tmp + 'whatsapp.ab -noapk com.whatsapp')
         except Exception as e : 
-            CustomPrint(e)
-        CustomPrint('Done backing up data.')
+            print(e)
+        print('Done backing up data.')
 
         #Reinstall WhatsApp
-        CustomPrint('Reinstallting original WhatsApp...')
+        print('Reinstallting original WhatsApp...')
         try : 
             os.system(adb + ' install -r -d ' + tmp + 'WhatsAppbackup.apk')
         except Exception as e : 
             print(e)
-            CustomPrint('Could not install WhatsApp, install by running \'restore_whatsapp.py\' or manually installing from Play Store.\nHowever if it crashes then you have to clear storage/clear data from settings => app settings => WhatsApp.')    
+            print('Could not install WhatsApp, install by running \'restore_whatsapp.py\' or manually installing from Play Store.\nHowever if it crashes then you have to clear storage/clear data from settings => app settings => WhatsApp.')    
 
-        #CustomPrint('Our work with device has finished.')
+        #print('Our work with device has finished.')
         
         if not (isJAVAInstalled): 
             print('Can not detect JAVA on system.')
