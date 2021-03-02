6664565585d0da4c9d77cbdaec89cf258b16a47f
Robert S <31773450+roberts01@users.noreply.github.com>
2021-03-02 10:08:31 +0100

    Update termux_dependencies.py

--- helpers/termux_dependencies.py
+++ helpers/termux_dependencies.py
@@ -9,13 +9,15 @@ def InstallTermuxDependencies() :
     os.system('termux-setup-storage')
     CustomPrint("Done. Installing required dependencies...")
     os.system('pkg install curl grep tar proot wget -y')
-    os.system('pip install termcolor')
-    os.system('wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh && bash InstallTools.sh')
-    try : 
+    os.system('pip install termcolor packaging')
+    #os.system('wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh && bash InstallTools.sh')
+    os.system('git clone https://github.com/MasterDevX/Termux-ADB.git && bash InstallTools.sh')
+    try:
         os.system('rm -r -f installjava') # Deleting any previous instance of installjava.
     except Exception as e : 
         pass
-    os.system('wget https://raw.githubusercontent.com/MasterDevX/java/master/installjava && sh installjava')
+    #os.system('wget https://raw.githubusercontent.com/MasterDevX/java/master/installjava && sh installjava')
+    os.system('git clone https://github.com/MasterDevX/java.git && sh installjava')
     os.system('proot login')
     CustomPrint("Connecting ADB with local device:")
     os.system('adb connect localhost')
