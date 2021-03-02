9427d9249a9251d085561818809fd04dce3773dd
Robert S <31773450+roberts01@users.noreply.github.com>
2021-03-02 11:35:52 +0100

    Update Termux.py

--- Termux.py
+++ Termux.py
@@ -3,19 +3,21 @@ from CustomCI import CustomPrint
 import os
 from subprocess import check_output
 from packaging import version
-import wget
 
 # Global variables
-appURLWhatsAppCDN = 'https://www.cdn.whatsapp.net/android/2.11.431/WhatsApp.apk'
+# appURLWhatsAppCDN = 'https://www.cdn.whatsapp.net/android/2.11.431/WhatsApp.apk'
 appURLWhatsCryptCDN = 'https://whatcrypt.com/WhatsApp-2.11.431.apk'
 
-def AfterConnect(ADBSerialId) : 
+def TermuxMode(ADBSerialId) : 
+    _deviceName= 'adb -s ' + ADBSerialId + ' shell getprop ro.product.model'
+    CustomPrint('Connected to ' + re.search("(?<=b')(.*)(?=\\\\n)", str(check_output(_deviceName.split()))).group(1) , 'green')
+
     _sdkVersionText = 'adb -s ' + ADBSerialId + ' shell getprop ro.build.version.sdk'
     SDKVersion = int(re.search('[0-9]{2,3}', str(check_output(_sdkVersionText.split()))).group(0))
     if (SDKVersion <= 13) : 
         CustomPrint('Unsupported device. This method only works on Android v4.0 or higer.', 'green')
         CustomPrint('Cleaning up temporary direcory.', 'green')
-        os.system('rm -rf tmp/*')
+        os.system('rm -r -f tmp/*')
         Exit()
     _waPathText = 'adb -s ' + ADBSerialId + ' shell pm path com.whatsapp'
     WhatsAppapkPath = re.search('(?<=package:)(.*)(?=apk)', str(check_output(_waPathText.split()))).group(1) + 'apk'
@@ -24,15 +26,8 @@ def AfterConnect(ADBSerialId) :
     contentLength = int(re.search("(?<=Content-Length:)(.*[0-9])(?=)", str(check_output('curl -sI http://www.cdn.whatsapp.net/android/2.11.431/WhatsApp.apk'.split()))).group(1))
     _versionNameText = 'adb -s ' + ADBSerialId + ' shell dumpsys package com.whatsapp'
     versionName = re.search("(?<=versionName=)(.*?)(?=\\\\n)", str(check_output(_versionNameText.split()))).group(1)
-    CustomPrint('WhatsApp V' + versionName + ' installed on device')
-    downloadAppFrom = appURLWhatsAppCDN if(contentLength == 18329558) else appURLWhatsCryptCDN
-    if (version.parse(versionName) > version.parse('2.11.431')) :
-        if not (os.path.isfile('helpers/LegacyWhatsApp.apk')) : 
-            CustomPrint('Downloading legacy WhatsApp V2.11.431 to helpers folder')
-            wget.download(downloadAppFrom, 'helpers/LegacyWhatsApp.apk')
-        else : 
-            CustomPrint('Found legacy WhatsApp V2.11.431 apk in helpers folder')
- 
+    
+    CustomPrint('WhatsApp Version' + versionName + ' installed on device') 
     return 1, SDKVersion, WhatsAppapkPath, versionName
 
 def Exit():
@@ -40,7 +35,4 @@ def Exit():
     os.system('adb kill-server')
     quit()
 
-def TermuxMode(ADBSerialId) : 
-    _deviceName= 'adb -s ' + ADBSerialId + ' shell getprop ro.product.model'
-    CustomPrint('Connected to ' + re.search("(?<=b')(.*)(?=\\\\n)", str(check_output(_deviceName.split()))).group(1) , 'green')
-    return AfterConnect(ADBSerialId)
+
